ac54c9f56a3be3186cd8f6e7cbe94e5232b53e73
boriratkk-boop <boriratkk@gmail.com>
2026-01-05 14:10:15 +0700

    Update cartPage.js

--- pages/cartPage.js
+++ pages/cartPage.js
@@ -2,13 +2,27 @@ class CartPage {
   constructor(page) {
     this.page = page;
     this.inven_item = page.locator('.inventory_item_name');
+    this.cartItems = page.locator('.cart_item');
+    this.removeButtons = page.locator('button[data-test^="remove-"]');
   }
 
   async verifyItem(testName) {
-    await this.inven_item.waitFor();
+    await this.inven_item.locator().waitFor();
     await this.page.screenshot({ path: `screenshots/${testName}-verify-cart.png` });
   }
+
+  async getCartItems() {
+    return await this.cartItems.elementHandles();
+  }
+
+  async removeAllProducts(testName) {
+    const count = await this.removeButtons.count();
+    for (let i = 0; i < count; i++) {
+      await this.removeButtons.nth(i).click();
+    }
+    await this.page.screenshot({ path: `screenshots/${testName}-removed-all.png` });
+  }
+  
 }
 
 module.exports = { CartPage };
-
