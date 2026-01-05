6ccb0d5fbecf8363f75389e6578c62c3e226553b
boriratkk-boop <boriratkk@gmail.com>
2026-01-05 11:31:18 +0700

    Update login.fc.spec.js

--- tests/functional/login.fc.spec.js
+++ tests/functional/login.fc.spec.js
@@ -8,8 +8,16 @@ test('@functional Input fields', async ({loginPage, page}) => {
     users.validUser.password,
     'fucntional-login'
   );
-  await expect(loginPage.username)
-    .toHaveValue(users.validUser.username);
-  await expect(loginPage.password)
-    .toHaveValue(users.validUser.password);
+  await expect(loginPage.username).toHaveValue(users.validUser.username);
+  await expect(loginPage.password).toHaveValue(users.validUser.password);
+});
+
+test('@functional Should show an error message if log in without a username', async ({loginPage, page}) => {
+  await loginPage.goto();
+  await loginPage.check_fields(
+    '',
+    users.validUser.password,
+    'fucntional-login'
+  );
+  await expect(loginPage.error).toHaveValue('Epic sadface: Username is required');
 });
