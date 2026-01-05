6bfdb0b6cc5dc706962fcde1df9caebce4c12dab
boriratkk-boop <boriratkk@gmail.com>
2026-01-05 15:47:10 +0700

    Update product.fc.spec.js

--- tests/functional/product.fc.spec.js
+++ tests/functional/product.fc.spec.js
@@ -8,10 +8,10 @@ test('@functional Adding all available products to the cart and then removing th
   await loginPage.login(users.validUser.username, users.validUser.password);
 
   // addAll
-  const expectedCount = await productsPage.addAllProducts('addAll-TC007');
+  const expectedCount = await productsPage.addAllProducts('TC007-addAll');
 
   // OpenCart
-  await productsPage.openCart('afterAddAll-TC007');
+  await productsPage.openCart('TC007-afterAddAll');
   
   // Get item
   const allItems = await cartPage.getCartItems();
@@ -20,9 +20,38 @@ test('@functional Adding all available products to the cart and then removing th
   await expect(allItems.length).toBe(expectedCount);
 
   // remove all products
-  await cartPage.removeAllProducts('afterRemoveAll-TC007');
+  await cartPage.removeAllProducts('TC007-afterRemoveAll');
 
   // verify cart is empty
   const finalItems = await cartPage.getCartItems();
   await expect(finalItems.length).toBe(0);
 });
+
+test('@functional Product should correctly sorts items from A to Z', async ({ loginPage, productsPage, page }) => {
+    // 1️⃣ Login
+    await loginPage.goto();
+    await loginPage.login(
+      users.validUser.username,
+      users.validUser.password
+    );
+  
+    // เลือก sort A → Z
+    await productsPage.sortByNameAToZ();
+  
+    // ดึงชื่อสินค้าทั้งหมดจากหน้า UI
+    const productNamesFromUI = await productsPage.getAllProductNames();
+  
+    // สร้าง list ที่ควรจะเป็น (เรียง A → Z)
+    const sortedNames = [...productNamesFromUI].sort((a, b) =>
+      a.localeCompare(b)
+    );
+  
+    // Assert ว่า UI เรียงถูกต้อง
+    await expect(productNamesFromUI).toEqual(sortedNames);
+  
+    // Screenshot เป็น evidence
+    await page.screenshot({
+      path: 'screenshots/functioncal/product/TC008-Select A-Z',
+      fullPage: true
+    });
+  });
