230ae9073c5cd066df64ca973ebdeca635016139
boriratkk-boop <boriratkk@gmail.com>
2026-01-06 09:50:10 +0700

    Update product.fc.spec.js

--- tests/functional/product.fc.spec.js
+++ tests/functional/product.fc.spec.js
@@ -1,74 +1,91 @@
-class ProductsPage {
-  constructor(page) {
-    this.page = page;
-    this.backpack = page.locator('#add-to-cart-sauce-labs-backpack');
-    this.cart_link = page.locator('.shopping_cart_link');
-    this.addButtons = page.locator('button[data-test^="add-to-cart-"]');
-    this.inven_item = page.locator('.inventory_item_name');
-    this.sortDropdown = page.locator('.product_sort_container');
-    this.productPrices = page.locator('.inventory_item_price');
+const { test, expect } = require('../../fixtures/testFixtures');
+const users = require('../../data/users');
+
+test('@functional Adding all available products to the cart and then removing them, verifying that the cart updates correctly', async ({ loginPage, productsPage, cartPage, page }) => {
+  await loginPage.goto();
+  await loginPage.login(users.validUser.username, users.validUser.password);
+  const expectedCount = await productsPage.addAllProducts('TC007-addAll');
+  await productsPage.openCart('TC007-afterAddAll');
+  const allItems = await cartPage.getCartItems();
+  await expect(allItems.length).toBe(expectedCount);
+  await cartPage.removeAllProducts('TC007-afterRemoveAll');
+  const finalItems = await cartPage.getCartItems();
+  await expect(finalItems.length).toBe(0);
+});
 
 
-  }
+test('@functional Product should correctly sorts items from A to Z', async ({ loginPage, productsPage, page }) => {
+    await loginPage.goto();
+    await loginPage.login(users.validUser.username,users.validUser.password);
+    const productNamesFromUI = await productsPage.getAllProductNames();
+    const sortedNames = [...productNamesFromUI].sort((a, b) => a.localeCompare(b));
+    await expect(productNamesFromUI).toEqual(sortedNames);
 
-  async addToCart(testName) {
-    await this.backpack.click();
-    await this.page.screenshot({ path: `screenshots/${testName}-add-to-cart.png`,
-    fullPage: true 
-  });
-  }
-
-  async openCart(testName) {
-    await this.cart_link.click();
-    await this.page.screenshot({ path: `screenshots/functional/product/${testName}-open-cart.png` ,
-    fullPage: true
-  });
-  }
-
-  async addAllProducts(testName) {
-    const totalProducts = await this.inven_item.count();
-
-  while (await this.addButtons.count() > 0) {
-    await this.addButtons.first().click();
-  }
-  
-    await this.page.screenshot({
-      path: `screenshots/functional/product/${testName}-added-all.png`,
+    await page.screenshot({
+      path: 'screenshots/functional/product/TC008-Select A-Z.png',
       fullPage: true
     });
+  });
 
-    return totalProducts;
+  test('@functional Product should correctly sorts items from Z to A', async ({ loginPage, productsPage, page }) => {
+    await loginPage.goto();
+    await loginPage.login( users.validUser.username,users.validUser.password);
+    await productsPage.sortByNameZToA();
+    const productNamesFromUI = await productsPage.getAllProductNames();
+    const sortedNames = [...productNamesFromUI].sort((a, b) =>b.localeCompare(a));
+    await expect(productNamesFromUI).toEqual(sortedNames);
 
-  }
+    await page.screenshot({
+      path: 'screenshots/functional/product/TC009-Select Z-A.png',
+      fullPage: true
+    });
+  });
 
-  async sortByNameAToZ() {
-    await this.sortDropdown.selectOption('az');
-  }
 
-  async sortByNameZToA() {
-    await this.sortDropdown.selectOption('za');
-  }
+  test('@functional Product should correctly sort items from Low to High price', async ({ loginPage, productsPage, page }) => {
+    await loginPage.goto();
+    await loginPage.login( users.validUser.username,users.validUser.password);
+    await productsPage.sortByPriceLowToHigh();
+    const pricesFromUI = await productsPage.getAllProductPrices();
+    const sortedPrices = [...pricesFromUI].sort((a, b) => a - b);
+    await expect(pricesFromUI).toEqual(sortedPrices);
 
-  async getAllProductNames() {
-    return await this.inven_item.allTextContents();
-  }
+    await page.screenshot({
+      path: 'screenshots/functional/product/TC010_sort_price_low_to_high.png',
+      fullPage: true
+    });
+  });
 
-  async sortByPriceLowToHigh() {
-    await this.sortDropdown.selectOption('lohi');
-  }
-  async sortByPriceHighToLow() {
-    await this.sortDropdown.selectOption('hilo');
-  }
-  async getAllProductPrices() {
-    const priceTexts = await this.productPrices.allTextContents();
-
-    // แปลง "$29.99" → 29.99 (number)
-    return priceTexts.map(price =>
-      Number(price.replace('$', ''))
+  test('@functional Product should correctly sort items from High to Low price', async ({ loginPage, productsPage, page }) => {
+    await loginPage.goto();
+    await loginPage.login(
+      users.validUser.username,
+      users.validUser.password
     );
-  }
-}
+    await productsPage.sortByPriceHighToLow();
+    const pricesFromUI = await productsPage.getAllProductPrices();
+    const sortedPrices = [...pricesFromUI].sort((a, b) => b - a);
+    await expect(pricesFromUI).toEqual(sortedPrices);
+
+    await page.screenshot({
+      path: 'screenshots/functional/product/TC011_sort_price_high_to_low.png',
+      fullPage: true
+    });
+  });
 
 
+  test('@functional Should navigate to the cart page when clicking the cart icon', async ({loginPage, productsPage ,cartPage ,page}) => {
+    await loginPage.goto();
+    await loginPage.login(
+      users.validUser.username,
+      users.validUser.password,);
 
-module.exports = { ProductsPage };
+    await productsPage.openCart();
+    await expect(cartPage.cartitle).toHaveText('Your Cart');
+
+    await page.screenshot({
+      path: 'screenshots/functional/product/TC011_when_clicking_the_cart_icon.png',
+      fullPage: true
+    });
+  
+  });
